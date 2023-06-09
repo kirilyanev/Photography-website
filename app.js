4875f72419d7034277fc8cb860c9e222f803a4dc
Kiril Yanev <kl_yanev@abv.bg>
2023-06-09 23:05:45 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,18 +2,26 @@
 
 const sections = Array.from(document.querySelectorAll('.main-section section'));
 const links = Array.from(document.querySelectorAll('a'));
+
 const overviewLink = links[1];
 const personalLink = links[2];
 const commissionedLink = links[3];
 const aboutLink = links[4];
 
-overviewLink.addEventListener('click', loadOverview);
+overviewLink.addEventListener('click', () => loadView(sections[0]));
+personalLink.addEventListener('click', () => loadView(sections[1]));
+commissionedLink.addEventListener('click', () => loadView(sections[2]));
+aboutLink.addEventListener('click', () => loadView(sections[3]));
 
-function loadOverview() {
+
+function loadView(section) {
+    hideAllViews();
+    section.style.display = 'block';
+}
+
+
+function hideAllViews() {
     sections.forEach(section => {
         section.style.display = 'none';
     })
-    sections[0].style.display = 'block';
 }
-
-console.log(sections);
